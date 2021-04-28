614937716ffb09057b1d56716d152b8042501b7f
JamesCQ <55759543+JamesCQ@users.noreply.github.com>
2021-04-28 20:58:24 +0200

    Metodos añadidos clase Tarjeta

--- ProyectoSupermercado/src/main/java/Main/Classes/Tarjeta.java
+++ ProyectoSupermercado/src/main/java/Main/Classes/Tarjeta.java
@@ -1,8 +1,8 @@
 package Main.Classes;
 
-
 import java.util.Calendar;
 import java.sql.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 /**
@@ -17,12 +17,19 @@ public class Tarjeta {
 
     public Tarjeta() {
     }
-
+    
     public Tarjeta(int numero, String Nombre_tarjeta, Calendar fecha_caducidad, String tipo_tarjeta) {
         this.setNumero(numero);
         this.setNombre_tarjeta(Nombre_tarjeta);
         this.setFecha_caducidad(fecha_caducidad);
         this.setTipo_tarjeta(tipo_tarjeta);
+    }
+    
+    public Tarjeta(Tarjeta t1) {
+        this.setNumero(t1.getNumero());
+        this.setNombre_tarjeta(t1.getNombre_tarjeta());
+        this.setFecha_caducidad(t1.getFecha_caducidad());
+        this.setTipo_tarjeta(t1.getTipo_tarjeta());
     }
 
     public int getNumero() {
@@ -56,12 +63,20 @@ public class Tarjeta {
     public void setTipo_tarjeta(String tipo_tarjeta) {
         this.tipo_tarjeta = tipo_tarjeta;
     }
+
+    @Override
+    public String toString() {
+        return "Tarjeta{" + "numero=" + numero + ", Nombre_tarjeta=" + Nombre_tarjeta + ", fecha_caducidad=" + fecha_caducidad + ", tipo_tarjeta=" + tipo_tarjeta + '}';
+    }
     
-    public static void anadirTarjeta(ArrayList <Tarjeta> ListaTarjeta){
+    public static void anadirTarjeta(int numero, String Nombre_tarjeta, Calendar fecha_caducidad, String tipo_tarjeta) throws SQLException{
+        Tarjeta t1=new Tarjeta(numero, Nombre_tarjeta, fecha_caducidad, tipo_tarjeta);
+        Herramientas.enviarComando("INSERT INTO Tarjeta Values("+t1.getNumero()+","+t1.getNombre_tarjeta()+","+t1.getFecha_caducidad()+","+t1.getTipo_tarjeta()+")");
+        Herramientas.cerrarConexion();
     }
 
-    public static void eliminarTarjeta(ArrayList <Tarjeta> ListaTarjeta){
+    public static void eliminarTarjeta(int numero)throws SQLException{
+        Herramientas.enviarComando("DELETE FROM Tarjeta WHERE numero_tarjeta = " +numero+ ";");
+        Herramientas.cerrarConexion();
     }
-    
-    
 }
