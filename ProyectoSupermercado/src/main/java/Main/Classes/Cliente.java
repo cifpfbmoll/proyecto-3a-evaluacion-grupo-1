a9e92647893ff77c30ff8b837efde0807cc9958f
afernandezr37 <afernandezr@cifpfbmoll.eu>
2021-05-04 17:16:52 +0200

    Hecho recoger cliente

--- ProyectoSupermercado/src/main/java/Main/Classes/Cliente.java
+++ ProyectoSupermercado/src/main/java/Main/Classes/Cliente.java
@@ -29,16 +29,17 @@ public class Cliente extends Persona {
     }
 
     public void eliminarPersona(Connection conexion, String nif) throws SQLException {
-        PreparedStatement borrar = conexion.prepareStatement("DELETE FROM employee_details WHERE salary = ?");
+        PreparedStatement borrar = conexion.prepareStatement("DELETE FROM cliente WHERE DNI_Cliente = ?");
         borrar.setString(1, nif);
-        borrar.executeQuery();
+        borrar.executeUpdate();
+        borrar.close();
     }
 
     public Cliente buscarCliente(String nif) {
         //TO DO and define
         return null;
     }
-   
+
     public static Cliente añadirPersona(String nombre, String apellido1, String apellido2, int edad, String nif, String cAutonoma, 
     String localidad, String cPostal, String direccion, String email, String contraseña, String rContraseña, String telefono) throws Excepciones{
 
@@ -47,6 +48,27 @@ public class Cliente extends Persona {
             throw e;
         }
         Cliente per = new Cliente(nombre, apellido1, apellido2, edad, nif, cAutonoma, localidad, cPostal, direccion, email, contraseña, telefono);
+        return per;
+    }
+
+    public static Cliente recogerCliente(Connection conexion, String nif) throws SQLException {
+        PreparedStatement sentencia = conexion.prepareStatement("SELECT * FROM cliente WHERE DNI_Cliente = ?");
+        sentencia.setString(1, nif);
+        ResultSet resultado = sentencia.executeQuery();
+        resultado.next();
+        String nombre = resultado.getString("Nombre_cliente");
+        String apellido1 = resultado.getString("Apellido_cliente_1");
+        String apellido2 = resultado.getString("Apellido_cliente_2");
+        int edad = resultado.getInt("Edad");
+        String cAutonoma = resultado.getString("Comunidad_autonoma");
+        String localidad = resultado.getString("Localidad");
+        String cPostal = resultado.getString("Codigo_postal");
+        String direccion = resultado.getString("Direccion");
+        String email = resultado.getString("Email");
+        String contraseña = resultado.getString("Contraseña");
+        Cliente per = new Cliente(nombre, apellido1, apellido2, edad, nif, cAutonoma, localidad, cPostal, direccion, email, contraseña, telefono);
+        resultado.close();
+        sentencia.close();
         return per;
     }
 
