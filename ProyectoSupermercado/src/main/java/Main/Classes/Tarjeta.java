9e1ad2be9892bf48d20ceb079e3a3f6485cd3f82
JamesCQ <55759543+JamesCQ@users.noreply.github.com>
2021-05-20 16:35:27 +0200

    Tarjeta

--- ProyectoSupermercado/src/main/java/Main/Classes/Tarjeta.java
+++ ProyectoSupermercado/src/main/java/Main/Classes/Tarjeta.java
@@ -4,6 +4,9 @@ import java.util.Calendar;
 import java.sql.*;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.*;
+import java.text.*;
+
 
 /**
  *
@@ -14,6 +17,7 @@ public class Tarjeta {
     private String Nombre_tarjeta;
     private Calendar fecha_caducidad;
     private String tipo_tarjeta;
+    private ArrayList <LineaCompra> listaTarjetas;
 
     public Tarjeta() {
     }
@@ -23,6 +27,7 @@ public class Tarjeta {
         this.setNombre_tarjeta(Nombre_tarjeta);
         this.setFecha_caducidad(fecha_caducidad);
         this.setTipo_tarjeta(tipo_tarjeta);
+        this.setListaTarjetas(listaTarjetas);
     }
     
     public Tarjeta(Tarjeta t1) {
@@ -30,6 +35,14 @@ public class Tarjeta {
         this.setNombre_tarjeta(t1.getNombre_tarjeta());
         this.setFecha_caducidad(t1.getFecha_caducidad());
         this.setTipo_tarjeta(t1.getTipo_tarjeta());
+    }
+
+    public ArrayList<LineaCompra> getListaTarjetas() {
+        return listaTarjetas;
+    }
+
+    public void setListaTarjetas(ArrayList<LineaCompra> listaTarjetas) {
+        this.listaTarjetas = listaTarjetas;
     }
 
     public int getNumero() {
@@ -69,7 +82,7 @@ public class Tarjeta {
         return "Tarjeta{" + "numero=" + numero + ", Nombre_tarjeta=" + Nombre_tarjeta + ", fecha_caducidad=" + fecha_caducidad + ", tipo_tarjeta=" + tipo_tarjeta + '}';
     }
     
-    public static void anadirTarjeta(int numero, String Nombre_tarjeta, Calendar fecha_caducidad, String tipo_tarjeta) throws SQLException{
+    public static void añadirTarjeta(int numero, String Nombre_tarjeta, Calendar fecha_caducidad, String tipo_tarjeta) throws SQLException{
         Tarjeta t1=new Tarjeta(numero, Nombre_tarjeta, fecha_caducidad, tipo_tarjeta);
         Herramientas.hacerSelect("INSERT INTO Tarjeta Values("+t1.getNumero()+","+t1.getNombre_tarjeta()+","+t1.getFecha_caducidad()+","+t1.getTipo_tarjeta()+")",true);
         Herramientas.cerrarConexion();
@@ -78,5 +91,17 @@ public class Tarjeta {
     public static void eliminarTarjeta(int numero)throws SQLException{
         Herramientas.modificarDatosTabla("DELETE FROM Tarjeta WHERE numero_tarjeta = " +numero+ ";",true);
         Herramientas.cerrarConexion();
+    }
+    
+    public static void insertarTarjeta(int numero, String Nombre_tarjeta, Calendar fecha_caducidad, String tipo_tarjeta, Connection conexion)throws SQLException{
+        PreparedStatement insertar = conexion.prepareStatement("INSERT INTO Tarjeta"
+                + " (Numero_tarjeta, Nombre_cliente, Fecha_caducidad, Tipo_tarjeta)"
+                + " VALUES"
+                + " (?, ?, ?, ?);");
+        insertar.setInt(1, numero);
+        insertar.setString(2, Nombre_tarjeta);
+        insertar.setObject(3, fecha_caducidad);
+        insertar.setString(4, tipo_tarjeta);
+        insertar.execute();
     }
 }
